f1e8e73faec84529f626dac01238cac498133776
fschweikhardt <schweikhardt.web@gmail.com>
2021-03-29 13:43:52 -0400

    updated configs

--- src/config.js
+++ src/config.js
@@ -3,4 +3,8 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_URL,
   API_TOKEN: process.env.API_TOKEN,
   NODE_ENV: process.env.NODE_ENV,
+  HOST: process.env.HOST_HEROKU,
+  USER: process.env.USER_HEROKU,
+  DATABASE: process.env.DATABASE_HEROKU,
+  PASSWORD: process.env.PASSWORD_PASSWORD,
 }
