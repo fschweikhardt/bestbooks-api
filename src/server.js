133ac7f723932037d8c7b3c78442a3fc9731371e
fschweikhardt <schweikhardt.web@gmail.com>
2021-03-29 13:47:38 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 const knex = require('knex')
 const app = require('./app')
-const { PORT, HOST, USER, DATABASE, PASSWORD } = require('./config')
+const { PORT } = require('./config')
 
 const db = knex({
   client: 'pg',
   connection: {
-    host : `${HOST}`,
-    user: `${USER}`,
-    database : `${DATABASE}`,
-    password : `${PASSWORD}`,
+    host : 'ec2-54-198-252-9.compute-1.amazonaws.com',
+    user: 'nobqqnmgwfztus',
+    database : 'dc4cgkjcdcgs46',
+    password : '2b2bd4cdcc4a7d4d5df414231c5f64070a304394b3816d9c22d89774ecc27fe8',
     ssl: {
       rejectUnauthorized: false
     }
