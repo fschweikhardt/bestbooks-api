1b057b6674f771de3d41d0f572c833eb17a586c4
fschweikhardt <schweikhardt.web@gmail.com>
2021-03-31 16:49:42 -0400

    Update book-fixtures.js

--- test/book-fixtures.js
+++ test/book-fixtures.js
@@ -1,61 +1,62 @@
-const DATA = {
-    "The Booker Prize" : [
-    {
-        year: 2020, 
-        title: "Shuggie Bain", 
-        author: "Douglas Stuart",
-        award: "The Booker Prize"
-    }, 
-    {
-        year: 2019, 
-        title: "The Testaments", 
-        author: "Magaret Atwood",
-        award: "The Booker Prize"
-    }, 
-    {
-        year: 2018, 
-        title: "Milkman", 
-        author: "Anna Burns",
-        award: "The Booker Prize"
-    }],
-    "National Book Award for Fiction" : [
-    {
-        year: 2020, 
-        title: "Interior Chinatown", 
-        author: "Charles Yu",
-        award: "National Book Award for Fiction"
-    }, 
-    {
-        year: 2019, 
-        title: "Trust Exercise", 
-        author: "Susan Choi",
-        award: "National Book Award for Fiction"
-    }, 
-    {
-        year: 2018, 
-        title: "The Friend", 
-        author: "Sigrid Nunez",
-        award: "National Book Award for Fiction"
-    }],
-    "The Pultizer Prize for Fiction" : [
-    {
-        year: 2020, 
-        title: "The Nickel Boys", 
-        author: "Colson Whitehead",
-        award: "The Pultizer Prize for Fiction"
-    }, 
-    {
-        year: 2019, 
-        title: "The Overstory", 
-        author: "Richard Powers",
-        award: "The Pultizer Prize for Fiction"
-    }, 
-    {
-        year: 2018, 
-        title: "Less", 
-        author: "Andrew Sean Greer",
-        award: "The Pultizer Prize for Fiction"
-    }]
+function makeBooksArray() {
+    return [
+        "The Booker Prize" : [
+        {
+            year: 2020, 
+            title: "Shuggie Bain", 
+            author: "Douglas Stuart",
+            award: "The Booker Prize"
+        }, 
+        {
+            year: 2019, 
+            title: "The Testaments", 
+            author: "Magaret Atwood",
+            award: "The Booker Prize"
+        }, 
+        {
+            year: 2018, 
+            title: "Milkman", 
+            author: "Anna Burns",
+            award: "The Booker Prize"
+        }],
+        "National Book Award for Fiction" : [
+        {
+            year: 2020, 
+            title: "Interior Chinatown", 
+            author: "Charles Yu",
+            award: "National Book Award for Fiction"
+        }, 
+        {
+            year: 2019, 
+            title: "Trust Exercise", 
+            author: "Susan Choi",
+            award: "National Book Award for Fiction"
+        }, 
+        {
+            year: 2018, 
+            title: "The Friend", 
+            author: "Sigrid Nunez",
+            award: "National Book Award for Fiction"
+        }],
+        "The Pultizer Prize for Fiction" : [
+        {
+            year: 2020, 
+            title: "The Nickel Boys", 
+            author: "Colson Whitehead",
+            award: "The Pultizer Prize for Fiction"
+        }, 
+        {
+            year: 2019, 
+            title: "The Overstory", 
+            author: "Richard Powers",
+            award: "The Pultizer Prize for Fiction"
+        }, 
+        {
+            year: 2018, 
+            title: "Less", 
+            author: "Andrew Sean Greer",
+            award: "The Pultizer Prize for Fiction"
+        }]
+    ]
 }
-
-export default DATA
+module.exports = { makeBooksArray }
