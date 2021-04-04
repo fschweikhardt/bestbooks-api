10114812e9899d740df06c1e3374ccb5ec6017bb
fschweikhardt <schweikhardt.web@gmail.com>
2021-04-04 16:51:37 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const knex = require('knex')
 const app = require('./app')
 const { PORT, HOST, USER, DATABASE, PASSWORD } = require('./config')
+//const { PORT, DATABASE_URL } = require('./config')
 
 const db = knex({
   client: 'pg',
@@ -12,6 +13,11 @@ const db = knex({
     ssl: { rejectUnauthorized: false }
   }
 })
+
+// const db = knex({
+//   client: 'pg',
+//   connection: DATABASE_URL
+// })
 
 app.set('db', db)
 
